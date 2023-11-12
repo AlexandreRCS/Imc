71d328f89184a33b0a82de019238111d7a576f30
AlexandreRCS <ale.maresias@gmail.com>
2023-11-11 21:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const form = document.querySelector('form')
 const weightInput = document.querySelector('#weight')
 const heightInput = document.querySelector('#height')
 
+const modalWrapper = document.querySelector('.modal-wrapper')
+const resultMessage = document.querySelector('h2')
+
+
 
 form.onsubmit = function(event){
     event.preventDefault()
