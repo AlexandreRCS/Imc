3593850d1625578e8d2d988cedb97dc46a037a5b
AlexandreRCS <ale.maresias@gmail.com>
2023-11-11 22:55:59 -0300

    script
    fazendo o JS e ajustando o CSS

--- script.js
+++ script.js
@@ -2,16 +2,52 @@ const form = document.querySelector('form')
 const weightInput = document.querySelector('#weight')
 const heightInput = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const resultMessage = document.querySelector('h2')
-
-
-
+const Modal ={
+    Wrapper : document.querySelector('.modal-wrapper'),
+    Message : document.querySelector('h2'),
+    BtnClose : document.querySelector('#btn-close'),
+    open(){
+        Modal.Wrapper.classList.add('open')
+    },
+    close(){
+        Modal.Wrapper.classList.remove('open')
+    }
+}
 form.onsubmit = function(event){
     event.preventDefault()
     
     const weight = weightInput.value
     const height = heightInput.value
+    
+    imc = 0
+    
+    imc = calculoImc(weight, height)
+    
+    let messege =  `O seu IMC é ${imc}`
+    
+    Modal.open()
+    
+    
+    Modal.Message.innerText = messege
 
-    console.log(weight, height)
 }
+
+
+
+function calculoImc(weight, height){
+    let imc = 0
+
+    imc =   weight / (height / 100 * 2)
+
+    return imc.toFixed()
+}
+
+
+
+Modal.BtnClose.onclick = function(){
+   Modal.close()
+}
+
+Modal.Wrapper.addEventListener('click', function(){
+    Modal.close()
+})
